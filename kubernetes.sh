728bb32b977320904d52c4da27ddc5df70c509d4
Gizem Pesen <pesengizem@gmail.com>
2022-11-07 21:50:35 +0300

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,13 +1,13 @@
 sudo apt-get update
-sudo apt-get install -y apt-transport-https
+sudo apt-get install -y apt-transport-https 
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
+su -c 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -'
+su -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update
 sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
-sudo kubeadm init
+su -c 'kubeadm init'
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
