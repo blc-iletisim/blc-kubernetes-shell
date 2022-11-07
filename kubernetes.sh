41c0be639078c804b902a1659f56d54dd5ab9925
Gizem Pesen <pesengizem@gmail.com>
2022-11-07 11:27:44 +0300

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y apt-transport-https -y
+sudo apt-get install -y apt-transport-https 
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
