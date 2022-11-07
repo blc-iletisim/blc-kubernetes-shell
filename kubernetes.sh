bccfff80cfca8baecdb2f39ed016e3dbba5f7406
gizempesen <pesengizem@gmail.com>
2022-11-07 08:19:39 +0000

    kubernetes shell

---
+++ kubernetes.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo su -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+kubectl proxy --address '0.0.0.0' --port=8002 --accept-hosts='.*'
+
