5cab73302e4aef54e1ec3e8713d75ba434dfc548
Gizem Pesen <pesengizem@gmail.com>
2022-11-07 21:47:14 +0300

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -7,8 +7,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
-kubeadm init
-mkdir -p $HOME/.kube
+sudo kubeadm init
+sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
