51c5ac4962e79d2e064ca6b40d055eff22f715f9
gizem <pesengizem999@gmail.com>
2023-01-06 12:56:14 +0300

    scripts moved

---
+++ postgresql/postgresql.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt install postgresql postgresql-contrib -y
+sudo apt-get update -y
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo docker-compose up -d
+
+
